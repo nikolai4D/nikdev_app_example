004668ba21a20766478590bec56c8a1d526e8d4b
feldspatt <mathieuantoine4@gmail.com>
2022-12-21 14:01:54 +0100

    added build option for index.mjs not to have a hash so it remains versionned.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     build: {
         minify: false,
+        rollupOptions: {
+            output: {
+                dir: 'dist',
+                format: 'module',
+                entryFileNames: 'index.mjs',
+            }
+        }
     }
 })
